7b45ce1d090ac2c6a085fd036f48f78f8e81889f
piotr <psekulski@student.agh.edu.pl>
2024-11-04 15:39:51 +0100

    added arguments for World.main()

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -39,42 +39,71 @@ import agh.ics.oop.OptionsParser;
 //    }
 //}
 //ex 16-20
+//public class World {
+//
+////    public static void run(MoveDirection[] directions) {
+////        for (MoveDirection direction : directions) {
+////            switch (direction) {
+////                case FORWARD:
+////                    System.out.println("Zwierzak idzie do przodu");
+////                    break;
+////                case BACKWARD:
+////                    System.out.println("Zwierzak idzie do tyłu");
+////                    break;
+////                case RIGHT:
+////                    System.out.println("Zwierzak skreca w prawo");
+////                    break;
+////                case LEFT:
+////                    System.out.println("Zwierzak skreca w lewo");
+////                    break;
+////            }
+////        }
+////    }
+//
+//
+////    public static void main(String[] args) {
+////        System.out.println("Start");
+////        List<MoveDirection> directions = OptionsParser.parse(args);
+//////        run(directions);
+////        System.out.println("Stop");
+////
+////        Vector2d position1 = new Vector2d(1,2);
+////        System.out.println(position1);
+////        Vector2d position2 = new Vector2d(-2,1);
+////        System.out.println(position2);
+////        System.out.println(position1.add(position2));
+////        Animal animal = new Animal();
+////        System.out.println(animal.toString());
+////
+////    }
+//
+//
+//    public static void main(String[] args) {
+//        List<MoveDirection> directions = OptionsParser.parse(args);
+//        List<Vector2d> positions = List.of(new Vector2d(2, 2), new Vector2d(3, 4));
+//        Simulation simulation = new Simulation(positions, directions);
+//        simulation.run(directions);
+//    }
+//}
+
 public class World {
-
-    public static void run(MoveDirection[] directions) {
-        for (MoveDirection direction : directions) {
-            switch (direction) {
-                case FORWARD:
-                    System.out.println("Zwierzak idzie do przodu");
-                    break;
-                case BACKWARD:
-                    System.out.println("Zwierzak idzie do tyłu");
-                    break;
-                case RIGHT:
-                    System.out.println("Zwierzak skreca w prawo");
-                    break;
-                case LEFT:
-                    System.out.println("Zwierzak skreca w lewo");
-                    break;
-            }
-        }
-    }
-
-
-
     public static void main(String[] args) {
-        System.out.println("Start");
-        List<MoveDirection> directions = OptionsParser.parse(args);
-//        run(directions);
-        System.out.println("Stop");
 
-        Vector2d position1 = new Vector2d(1,2);
-        System.out.println(position1);
-        Vector2d position2 = new Vector2d(-2,1);
-        System.out.println(position2);
-        System.out.println(position1.add(position2));
-        Animal animal = new Animal();
-        System.out.println(animal.toString());
+        // Animal manipulation
+        Animal Monkey = new Animal();
+        System.out.println(Monkey);
+        Monkey.move(MoveDirection.FORWARD);
+        Monkey.move(MoveDirection.FORWARD);
+        Monkey.move(MoveDirection.FORWARD);
+        Monkey.move(MoveDirection.FORWARD);
+        Monkey.move(MoveDirection.FORWARD);
+        Monkey.move(MoveDirection.FORWARD);
+        System.out.println(Monkey);
+        List<MoveDirection> directions = OptionsParser.parse(args);
+        List<Vector2d> positions = List.of(new Vector2d(2,2), new Vector2d(3,4));
+        Simulation simulation = new Simulation(positions, directions);
+        simulation.run();
+
 
     }
 }
