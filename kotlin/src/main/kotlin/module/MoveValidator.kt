f370bb8d7c34110b395be94ef1d59747c83bb445
lil piter <108008710+Seku1@users.noreply.github.com>
2025-01-18 23:43:14 +0100

    Revert "Lab2 proj do oceny"

--- kotlin/src/main/kotlin/module/MoveValidator.kt
+++
@@ -1,5 +0,0 @@
-package org.example.module
-
-interface MoveValidator {
-    fun canMoveTo(position: Vector2d) : Boolean
-}
--- kotlin/src/main/kotlin/module/util/IncorrectPositionException.kt
+++
@@ -1,5 +0,0 @@
-package org.example.module.util
-
-import org.example.module.Vector2d
-
-class IncorrectPositionException(val position: Vector2d) : Exception("Position: $position is already occupied")
